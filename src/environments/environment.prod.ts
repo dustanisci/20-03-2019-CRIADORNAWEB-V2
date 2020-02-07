2b54ede3a4210b2c2b4846ce19d717735a97dbe5
Eduardo Stanisci <dustanisci@outlook.com>
2020-02-07 10:32:28 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiPortfolio : "http://criadornaweb.com.br/servicos/webservice/view/listarprojetos.php",
+  apiPortfolio : "https://www.mocky.io/v2/5e3434273000008245d9637d",
   apiWhatsapp : "https://api.whatsapp.com/send?phone=5511959662923&text="
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiPortfolio : "http://criadornaweb.com.br/servicos/webservice/view/listarprojetos.php",
+  apiPortfolio : "https://www.mocky.io/v2/5e3434273000008245d9637d",
   apiWhatsapp : "https://api.whatsapp.com/send?phone=5511959662923&text="
 };
 
